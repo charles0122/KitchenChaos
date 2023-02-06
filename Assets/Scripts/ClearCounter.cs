703f15a19ad567a1c5bbb79d9e9f6fdba53b104d
charles <zainai311@qq.com>
2023-02-06 13:14:06 +0800

    交互生成物品

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
     public void Interact() {
         Debug.Log("Interact");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab,counterTopPoint);
+        kitchenObjectTransform.localPosition = Vector3.zero;
     }
 }
