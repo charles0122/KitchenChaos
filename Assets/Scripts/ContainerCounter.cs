5fc06ad29a8eb52ebee7da6339399bce95e7167d
charles <zainai311@qq.com>
2023-02-07 09:58:14 +0800

    feat: 增加容器柜台动画效果

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ContainerCounter : BaseCounter, IKitchenObjectParent {
 
     // ³ø·¿ÎïÆ·SO
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public event EventHandler OnPlayerGrabbedObject;
     
 
     public override void  Interact(Player player) {
         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
         kitchenObjectTransform.GetComponent<KitchenObject>().SetkitchenObjectParent(player);
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
 
     
--- Assets/Scripts/ContainerCounterVisual.cs
+++ Assets/Scripts/ContainerCounterVisual.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContainerCounterVisual : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class ContainerCounterVisual : MonoBehaviour {
+    // ¶¯»­
+    private Animator animator;
+    private const string OPEN_CLOSE = "OpenClose";
+
+
+    [SerializeField] private ContainerCounter containerCounter;
+
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        containerCounter.OnPlayerGrabbedObject += ContainerCounter_OnPlayerGrabbedObject;
+    }
+
+    private void ContainerCounter_OnPlayerGrabbedObject(object sender, System.EventArgs e) {
+        animator.SetTrigger(OPEN_CLOSE);
     }
 }
