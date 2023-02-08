af436065b2f90301b26ecda5915f7241819366ce
charles <zainai311@qq.com>
2023-02-08 11:11:49 +0800

    feat: 垃圾桶功能、初始化煮菜柜台

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TrashCounter : BaseCounter{
-       
+    public override void Interact(Player player) {
+        if (player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
 }
