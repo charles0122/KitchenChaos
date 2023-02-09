6648183c58bd0b41c7dbb95d314a3c03068c37ea
charles <zainai311@qq.com>
2023-02-09 14:10:14 +0800

    pref: 优化单个菜谱显示效果

--- Assets/Scripts/UI/RecipeSingleUI.cs
+++ Assets/Scripts/UI/RecipeSingleUI.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Localization;
 using UnityEngine.Localization.Settings;
 
 public class RecipeSingleUI : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI recipeName;
+    [SerializeField] private Transform iconsContainer;
+    [SerializeField] private Transform iconTemplate;
+
+    private void Awake() {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
     public void SetRecipeSO(RecipeSO recipeSO) {
         recipeName.text = recipeSO.recipeName;
-        
+        // 销毁容器下除模板以外的图片
+        foreach (Transform child in iconsContainer) {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO child in recipeSO.kitchenObjectSOList) {
+            Transform iconTransform = Instantiate(iconTemplate, iconsContainer);
+            iconTransform.gameObject.SetActive(true);
+
+            iconTransform.GetComponent<Image>().sprite = child.sprite;
+        }
     }
-}
+}   
